b1f58644ef62e95d83a296a3580e645e7f36864c
Meng Wu <kaiwu.email.unc.edu>
2023-04-29 04:24:29 -0400

    Add status code to all urls

--- server.js
+++ server.js
@@ -15,26 +15,34 @@ app.get('/app', function (req, res) {
 })
 
 app.get('/app/rps', function (req, res) {
+    res.status(200)
     res.send(rps())
 })
 
 app.get('/app/rpsls', function (req, res) {
+    res.status(200)
     res.send(rpsls())
 })
 
 app.get('/app/rps/play', function (req, res) {
+    res.status(200)
     res.send(rps(req.query.shot))
 })
 
 app.get('/app/rpsls/play', function (req, res) {
+    res.status(200)
     res.send(rpsls(req.query.shot))
 })
 
 app.post('/app/rps/play/:shot', function (req, res) {
+    res.status(200)
     res.send(rps(req.params.shot))
 })
 
 app.post('/app/rpsls/play/:shot', function (req, res) {
+    res.status(200)
     res.send(rpsls(req.params.shot))
 })
+
+
 
