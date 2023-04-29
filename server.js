b521011b57ff4eefcc215aa682c0d1e00ff2429d
Meng Wu <kaiwu.email.unc.edu>
2023-04-29 04:17:19 -0400

    Add get get requests for blank and playing

---
+++ server.js
@@ -0,0 +1,33 @@
+#!/user/bin/env node
+
+import minimist from 'minimist';
+import express from 'express';
+import { rps, rpsls } from './lib/rpsls.js';
+
+const args = minimist(process.argv.slice(2), {string: ['port'],
+                                              default: {'port': '5000'}});
+const app = express();
+app.use(express.json())
+
+app.get('/app', function (req, res) {
+    res.status(200)
+    res.send('200 OK')
+})
+
+app.get('/app/rps', function (req, res) {
+    res.send(rps())
+})
+
+app.get('/app/rpsls', function (req, res) {
+    res.send(rpsls())
+})
+
+app.get('/app/rps/play', function (req, res) {
+    res.send(rps(req.query.shot))
+})
+
+app.get('/app/rpsls/play', function (req, res) {
+    res.send(rpsls(req.query.shot))
+})
+
+
